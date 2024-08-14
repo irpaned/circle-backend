2bf8dd15015688ab7687be190b9d0a83f8ef2227
irpaned <muhammadirfan2823@gmail.com>
2024-08-14 15:02:52 +0700

    test

--- postinstall.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require("child_process");
-
-try {
-  execSync("prisma generate", { stdio: "inherit" });
-  execSync("prisma migrate deploy", { stdio: "inherit" });
-} catch (error) {
-  console.error("Failed to run postinstall script", error);
-  process.exit(1);
-}
